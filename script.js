79b43c98b8c9815d73a4327ed648d0926252b09e
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-11-06 13:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,19 @@
-function convertToRoman(num) {
-  	 //your code here
-	  if (typeof num !== 'number' || num <= 0 || num > 3999) {
-    return 'Invalid input: Please enter a positive integer between 1 and 3999.';
-  }
+
 
   // Define the Roman numeral symbols and their corresponding integer values
-  const romanSymbols = [
-    { value: 1000, symbol: 'M' },
-    { value: 900, symbol: 'CM' },
-    { value: 500, symbol: 'D' },
-    { value: 400, symbol: 'CD' },
-    { value: 100, symbol: 'C' },
-    { value: 90, symbol: 'XC' },
-    { value: 50, symbol: 'L' },
-    { value: 40, symbol: 'XL' },
-    { value: 10, symbol: 'X' },
-    { value: 9, symbol: 'IX' },
-    { value: 5, symbol: 'V' },
-    { value: 4, symbol: 'IV' },
-    { value: 1, symbol: 'I' }
-  ];
-
-  let romanNumeral = '';
-
-  // Iterate through the romanSymbols array
-  for (let i = 0; i < romanSymbols.length; i++) {
-    // While the input number is greater than or equal to the current symbol's value
-    while (num >= romanSymbols[i].value) {
-      // Append the symbol to the result string
-      romanNumeral += romanSymbols[i].symbol;
-      // Subtract the symbol's value from the input number
-      num -= romanSymbols[i].value;
-    }
+  function integerToRoman(num) {
+  if (!Number.isInteger(num) || num < 1 || num > 3999) {
+    return 'Invalid input';
   }
-  return romanNumeral;
-
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-console.log(convertToRoman(14));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman;
+  
+  const thousands = ['', 'M', 'MM', 'MMM'];
+  const hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+  const tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+  const ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+  
+  return thousands[Math.floor(num / 1000)] +
+         hundreds[Math.floor((num % 1000) / 100)] +
+         tens[Math.floor((num % 100) / 10)] +
+         ones[num % 10];
+  }
+console. log(integerToRoman(14));
