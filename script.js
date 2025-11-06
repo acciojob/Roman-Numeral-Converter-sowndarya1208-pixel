969c76f659f47812648d99ff4a6da5271ff68596
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-11-06 09:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	 //your code here
+	  if (typeof num !== 'number' || num <= 0 || num > 3999) {
+    return 'Invalid input: Please enter a positive integer between 1 and 3999.';
+  }
 
-  //your code here
+  // Define the Roman numeral symbols and their corresponding integer values
+  const romanSymbols = [
+    { value: 1000, symbol: 'M' },
+    { value: 900, symbol: 'CM' },
+    { value: 500, symbol: 'D' },
+    { value: 400, symbol: 'CD' },
+    { value: 100, symbol: 'C' },
+    { value: 90, symbol: 'XC' },
+    { value: 50, symbol: 'L' },
+    { value: 40, symbol: 'XL' },
+    { value: 10, symbol: 'X' },
+    { value: 9, symbol: 'IX' },
+    { value: 5, symbol: 'V' },
+    { value: 4, symbol: 'IV' },
+    { value: 1, symbol: 'I' }
+  ];
+
+  let romanNumeral = '';
+
+  // Iterate through the romanSymbols array
+  for (let i = 0; i < romanSymbols.length; i++) {
+    // While the input number is greater than or equal to the current symbol's value
+    while (num >= romanSymbols[i].value) {
+      // Append the symbol to the result string
+      romanNumeral += romanSymbols[i].symbol;
+      // Subtract the symbol's value from the input number
+      num -= romanSymbols[i].value;
+    }
+  }
+  return romanNumeral;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(14));
 
 
 
